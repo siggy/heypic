1a553ec07fd585994ebdbf0c81e0fe57437b6c54
Andrew Seigner <andrew@sig.gy>
2015-04-12 21:18:40 -0700

    broke js/css into separate files

---
+++ public/heypic.js
@@ -0,0 +1,59 @@
+var map         = null;
+var markers     = [];
+var MAX_MARKERS = 100;
+var infoWindow  = new google.maps.InfoWindow();
+
+function initialize() {
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: { lat: 30, lng: 0},
+    zoom: 2
+  });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+var ws = new WebSocket("ws://localhost:8080");
+
+ws.onopen = function(evt) {
+  console.log("Connection open. Sending message...");
+  ws.send("Hello WebSockets!");
+};
+
+ws.onmessage = function(evt) {
+  var json = JSON.parse(evt.data);
+
+  var marker = new google.maps.Marker({
+    position: {lat: json.heypic.lat, lng: json.heypic.lon},
+    map: map,
+    animation: google.maps.Animation.DROP,
+    title: json.tweet.text,
+    icon: {
+      url: json.tweet.user.profile_image_url_https,
+      size: new google.maps.Size(20, 20),
+      origin: new google.maps.Point(0,0),
+      anchor: new google.maps.Point(0, 20)
+    }
+  });
+
+  markers.push(marker);
+  if (markers.length > MAX_MARKERS) {
+    markers.shift().setMap(null);
+  }
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infoWindow.setContent(
+      '<a target="_blank" href=\"' + json.heypic.img_url + '\">' +
+        '<img src=\"' + json.heypic.img_url +
+          ':thumb\" alt="original image" title="original image">' +
+      '</a>' +
+      '<div>' + json.tweet.text +'</div>');
+    infoWindow.open(map, marker);
+  });
+};
+
+ws.onclose = function(evt) {
+  console.log("Connection closed.");
+};
+
+ws.onerror = function(err) {
+  console.log(err.name + " => " + err.message);
+}
